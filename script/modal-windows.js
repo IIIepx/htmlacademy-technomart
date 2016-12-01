f0366158ff40c4cc6cbe3d293e9c865caf016d9d
Marchenko Maxim <slayer.jk@mail.ru>
2016-12-01 16:37:58 +0600

    Work on header top-menu. Experiment - added jQuery.

---
+++ script/modal-windows.js
@@ -0,0 +1,8 @@
+'use strict';
+
+$(document).ready( function() {
+  $('.nav-menu__element').on('click', function(){
+    /*$(this).toggleClass('nav-menu__js_active');*/
+    /*$(this).fadeOut(3000);*/
+  });
+});
