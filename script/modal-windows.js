14cf42061ee14063983762571a43a89c42cae497
Marchenko Maxim <slayer.jk@mail.ru>
2016-12-06 16:44:40 +0600

    Done(incl. PP) with header top-menu. Started header user-menu(middle section).

--- script/modal-windows.js
+++ script/modal-windows.js
@@ -1,8 +1,8 @@
 'use strict';
 
 $(document).ready( function() {
-  $('.nav-menu__element').on('click', function(){
-    /*$(this).toggleClass('nav-menu__js_active');*/
+  $('.nav-menu__element_js').on('click', function(){
+    $(this).toggleClass('nav-menu__js_active');
     /*$(this).fadeOut(3000);*/
   });
 });
