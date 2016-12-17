11732985de969195b27bda0e39ae500fb1bbec40
Marchenko Maxim <slayer.jk@mail.ru>
2016-12-17 16:45:06 +0600

    Header - done(incl. mass of pp, styleguide.)

--- script/modal-windows.js
+++ script/modal-windows.js
@@ -1,8 +1,9 @@
 'use strict';
 
 $(document).ready( function() {
-  $('.nav-menu__element_js').on('click', function(){
-    $(this).toggleClass('nav-menu__js_active');
+  $(document).on('click', '.user-menu__button', function(){
+    $(this).css('background-color', 'red');
+    $('.user-menu__li_login').append('<div class="item-test">TEST</div>');
     /*$(this).fadeOut(3000);*/
   });
 });
