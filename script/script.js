d488688b6c74a8fac6b7f72ee8b3ad593e43ff3d
Marchenko Maxim <slayer.jk@mail.ru>
2017-01-25 16:06:43 +0600

    Work on Index-Services section; added meta viewport in head.

--- script/script.js
+++ script/script.js
@@ -1,24 +1,27 @@
-
-"use strict";
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
 
 $(document).ready(function () {
+  "use strict";
 
-// slider-code----------------------------------------
+  // slider-code----------------------------------------
   $.fn.sliderSwitch = function () {
     $('.items-slider__slides').fadeToggle(1000);
   };
-  
+
   $(document).on('click', '.items-slider__switch , .items-slider__icon-font-switch', function () {
     $(this).sliderSwitch();
   });
-  
+
   setInterval($.fn.sliderSwitch, 4000);
-  
-// va-tests---------------------------------------------
-    $('.top-menu__logo').click(function () {
-      $(this).effect("bounce", {times: 3}, "slow");
-      
-    });
-// -----------------------------------------------------  
-  
+
+  // va-tests---------------------------------------------
+  $('.top-menu__logo').click(function () {
+    $(this).effect("bounce", {
+      times: 3
+    }, "slow");
+
+  });
+  // -----------------------------------------------------  
+
 });
